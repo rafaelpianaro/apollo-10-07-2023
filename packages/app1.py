eb0cd671b37640b231cb2d877d465c9f3b5eba15
Rafael Pianaro <rafaelpianaro@gmail.com>
2023-07-10 12:20:35 -0300

    feat: question 1

--- packages/app1.py
+++ packages/app1.py
@@ -1,7 +1,44 @@
 from fastapi import FastAPI
+import requests
+import csv
 
 app = FastAPI()
 
 @app.get("/")
-async def read_root():
-    return {"message": "Olá do Projeto 1!"}
+
+def gerar_arquivo_csv():
+    url = "https://pypi.org/pypi/sampleproject/json"
+
+    # Faz a solicitação GET para a API
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # Extrai os dados necessários do JSON recebido
+        info = data["info"]
+        nome = info["name"]
+        versao = info["version"]
+        autor = info["author"]
+        descricao = info["summary"]
+
+        # Define o nome do arquivo CSV
+        nome_arquivo = f"{nome}_{versao}.csv"
+
+        # Cria o arquivo CSV e escreve os dados
+        with open(nome_arquivo, "w", newline="") as arquivo_csv:
+            writer = csv.writer(arquivo_csv)
+            writer.writerow(["Nome", "Versão", "Autor", "Descrição"])
+
+            # Escreve os registros limitando a 50
+            registros_gravados = 0
+            while registros_gravados < 50:
+                writer.writerow([nome, versao, autor, descricao])
+                registros_gravados += 1
+
+        print(f"Arquivo CSV '{nome_arquivo}' gerado com sucesso.")
+    else:
+        print("Não foi possível acessar a API.")
+
+# Chamada da função para gerar o arquivo CSV
+gerar_arquivo_csv()
